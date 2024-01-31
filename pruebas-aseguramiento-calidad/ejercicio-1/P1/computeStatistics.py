ac7451291e58d17ea59751fded6fbcc1a95f874f
DiegoMont <diegomonmar@hotmail.com>
2024-01-31 00:01:04 -0600

    Primer avance tarea pruebas

---
+++ pruebas-aseguramiento-calidad/ejercicio-1/P1/computeStatistics.py
@@ -0,0 +1,83 @@
+"""Tool to get statistics of numbers in a file
+"""
+import sys
+import time
+
+
+def calculate_variance(sample: list, sample_mean: float) -> float:
+    """Calculates the variance of a list of numbers.
+    """
+    e = 0
+    for xi in sample:
+        e += (xi - sample_mean) ** 2
+    return e / len(sample)
+
+class Counter:
+    """Class that counts how many ocurrences of elements have appeared.
+    """
+    def __init__(self):
+        self.__elements: dict[float, int] = {}
+        self.__mode: float | None = None
+
+    def update(self, number: float):
+        """Add number ocurrence to counter
+        """
+        if not number in self.__elements:
+            self.__elements[number] = 0
+        self.__elements[number] += 1
+        if self.__mode is None:
+            self.__mode = number
+        if self.__elements[number] > self.__elements[self.__mode]:
+            self.__mode = number
+
+    def get_most_common(self) -> float | None:
+        """Gets the number that appeared the most.
+        """
+        return self.__mode
+
+
+if __name__ == "__main__":
+    start = time.time()
+    filename = sys.argv[1]
+    numbers = []
+    MEAN = 0
+    element_counter = Counter()
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
+            try:
+                num = float(line)
+                MEAN += num
+                numbers.append(num)
+                element_counter.update(num)
+            except ValueError:
+                print(f"Error: {line} is not a number")
+    NUMBER_COUNT = len(numbers)
+    MEAN /= NUMBER_COUNT
+    numbers = sorted(numbers)
+    ODD_AMOUNT_OF_NUMBERS = NUMBER_COUNT % 2 == 1
+    MID_INDEX = NUMBER_COUNT // 2
+    if ODD_AMOUNT_OF_NUMBERS:
+        median = numbers[MID_INDEX]
+    else:
+        median = (numbers[MID_INDEX] + numbers[MID_INDEX - 1]) / 2
+    mode = element_counter.get_most_common()
+    variance = calculate_variance(numbers, MEAN)
+    std_deviation = variance ** 0.5
+    elapsed_time = time.time() - start
+    count_report = f"COUNT: {NUMBER_COUNT:10.3f}\n"
+    mean_report = f"MEAN: {MEAN:10.3f}\n"
+    median_report = f"MEDIAN: {median:10.3f}\n"
+    MODE_REPORT = "MODE: " + ("N/A" if mode is None else str(mode)) + "\n"
+    variance_report = f"VARIANCE: {variance:10.3f}\n"
+    std_deviation_report = f"STD_DEV: {std_deviation:10.3f}\n"
+    time_report = f"EXECUTION TIME: {elapsed_time:10.3f}s"
+    statistics_report = count_report \
+        + mean_report \
+        + median_report \
+        + MODE_REPORT \
+        + std_deviation_report \
+        + variance_report \
+        + time_report
+    print(statistics_report)
+    with open("StatisticsResults.txt", "w", encoding="utf-8") as results_file:
+        results_file.write(statistics_report)
---
+++ pruebas-aseguramiento-calidad/ejercicio-1/P2/convertNumbers.py
@@ -0,0 +1,55 @@
+"""Prints decimal, binary and hexadecimal representation of a number in a file
+"""
+import sys
+import time
+
+
+def to_binary(decimal_number: int) -> str:
+    """Returns the binary representation of the decimal number
+    """
+    if decimal_number == 0:
+        return "0"
+    bits = []
+    while decimal_number > 0:
+        digit = str(decimal_number % 2)
+        bits.insert(0, digit)
+        decimal_number //= 2
+    bin_representation = "".join(bits)
+    return bin_representation
+
+
+def to_hex(decimal_number: int) -> str:
+    """Returns the hexadecimal representation of the decimal number
+    """
+    if decimal_number == 0:
+        return "0"
+    digit_map = ['0', '1', '2', '3', '4', '5', '6', '7',
+                 '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+    digits = []
+    while decimal_number > 0:
+        digit = digit_map[decimal_number % 16]
+        digits.insert(0, digit)
+        decimal_number //= 16
+    hex_representation = "".join(digits)
+    return hex_representation
+
+
+if __name__ == "__main__":
+    start = time.time()
+    filename = sys.argv[1]
+    CONVERSION_RESULTS = ""
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
+            try:
+                num = int(line)
+                BINARY = to_binary(num)
+                HEXADECIMAL = to_hex(num)
+                CONVERSION_RESULTS += f"{num}   {BINARY}   {HEXADECIMAL}\n"
+            except ValueError:
+                CONVERSION_RESULTS += f"{line}   #VALUE!   #VALUE!\n"
+    elapsed_time = time.time() - start
+    time_report = f"EXECUTION TIME: {elapsed_time:10.3f}s"
+    CONVERSION_RESULTS += time_report
+    print(CONVERSION_RESULTS)
+    with open("ConvertionResults.txt", "w", encoding="utf-8") as results_file:
+        results_file.write(CONVERSION_RESULTS)
