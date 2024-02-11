1c726487ba0cf43a40c83f6c2eac68ae028a3ecf
DiegoMont <diegomonmar@hotmail.com>
2024-02-11 00:07:19 -0600

    Fix ejercicio 2 Pylint problems

--- pruebas-aseguramiento-calidad/ejercicio-2/compute_sales.py
+++ pruebas-aseguramiento-calidad/ejercicio-2/compute_sales.py
@@ -1,66 +1,55 @@
+"""This module can parse the product info of products stored as JSON files and
+compute the total cost of product purchases stored in another JSON file
+"""
+
 import json
 import sys
 import time
 
 
-class Product:
-    def __init__(
-            self,
-            title: str,
-            type: str,
-            description: str,
-            filename: str,
-            height: float,
-            width: float,
-            price: float,
-            rating: int):
-        self.title: str = title
-        self.type: str = type
-        self.description: str = description
-        self.filename: str = filename
-        self.height: float = height
-        self.width: float = width
-        self.price: float = price
-        self.rating: int = rating
-
-
-class ProductPurchase:
-    def __init__(self, product: Product, quantity: int):
-        self.product: Product = product
-        self.quantity: int = quantity
-
-
 class Sale:
+    """Class that stores the purchased items in a sale. The `total_cost` class
+    variable holds the total cost of all the purchases accross `Sale`
+    instances.
+    """
     total_cost = 0
 
     def __init__(self):
-        self.purchased_products: dict[str, ProductPurchase] = {}
         self.total: float = 0
 
-    def addPurchase(self, product: Product, quantity: int):
-        if product not in self.purchased_products:
-            self.purchased_products[product.title] = ProductPurchase(
-                product, quantity)
-        subtotal = product.price * quantity
+    def add_purchase(self, product: dict, quantity: int):
+        """Computes the cost of the purchase and adds it to the `total_cost` of
+        all `Sale`s
+        """
+        subtotal = product['price'] * quantity
         Sale.total_cost += subtotal
         self.total += subtotal
 
+    def get_total(self) -> float:
+        """Gets the total cost of the sale
+        """
+        return self.total
 
-def load_products_file(file_path: str) -> dict[str, Product]:
+
+def load_products_file(file_path: str) -> dict[str, dict]:
+    """Parses a JSON file containing info about products
+    """
     products = {}
-    with open(file_path) as file:
+    with open(file_path, encoding="utf-8") as file:
         content = json.load(file)
-    for object in content:
-        products[object["title"]] = Product(**object)
+    for product in content:
+        products[product["title"]] = product
     return products
 
 
 def load_sales_file(
         file_path: str,
-        products: dict[str, Product]
+        products: dict[str, dict]
         ) -> dict[int, Sale]:
+    """Parses a JSON file containing info about sales
+    """
     sales = {}
-    with open(file_path) as file:
+    with open(file_path, encoding="utf-8") as file:
         content = json.load(file)
     for purchase in content:
         if purchase["SALE_ID"] not in sales:
@@ -69,7 +58,7 @@ def load_sales_file(
         try:
             purchased_product = products[purchase["Product"]]
             quantity = purchase["Quantity"]
-            sale.addPurchase(purchased_product, quantity)
+            sale.add_purchase(purchased_product, quantity)
         except KeyError:
             print(f"No está registrado el producto {purchase['Product']}")
     return sales
@@ -77,11 +66,11 @@ def load_sales_file(
 
 if __name__ == "__main__":
     start = time.time()
-    products = load_products_file(sys.argv[1])
-    sales = load_sales_file(sys.argv[2], products)
+    available_products = load_products_file(sys.argv[1])
+    purchases = load_sales_file(sys.argv[2], available_products)
     elapsed_time = time.time() - start
     output = f"Total cost: ${Sale.total_cost:.2f}\n"
     output += f"EXECUTION TIME: {elapsed_time:10.3f}s"
     print(output)
-    with open("SalesResults.txt", "w") as results_file:
+    with open("SalesResults.txt", "w", encoding="utf-8") as results_file:
         results_file.write(output)
