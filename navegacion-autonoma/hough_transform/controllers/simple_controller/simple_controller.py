05c8e159d00700dea0fedfa8a21a065df64f936a
DiegoMont <diegomonmar@hotmail.com>
2024-05-05 18:08:34 -0600

    Using lines angle to set car heading

--- navegacion-autonoma/hough_transform/controllers/simple_controller/simple_controller.py
+++ navegacion-autonoma/hough_transform/controllers/simple_controller/simple_controller.py
@@ -2,6 +2,7 @@
 """
 
 import math
+import statistics
 
 import cv2
 from controller import Display, Camera
@@ -17,20 +18,6 @@ def get_image(camera):
     return image
 
 
-def display_image(display, image):
-    # Image to display
-    image_rgb = np.dstack((image, image,image,))
-    # Display image
-    image_ref = display.imageNew(
-        image_rgb.tobytes(),
-        Display.RGB,
-        width=image_rgb.shape[1],
-        height=image_rgb.shape[0],
-    )
-    display.imagePaste(image_ref, 0, 0, False)
-    display.imageDelete(image_ref)
-
-
 def main():
     robot = Car()
     driver = Driver()
@@ -41,36 +28,52 @@ def main():
     assert isinstance(camera, Camera)
     camera.enable(timestep)
     display_view = Display("display_image")
-    speed = 5
+    speed = 10
     while robot.step() != -1:
         image = get_image(camera)
         grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        borders_mask = cv2.Canny(grey_image, 100, 200, None, 3)
+        blurred_image = cv2.GaussianBlur(grey_image, ksize=(5, 5), sigmaX=0, sigmaY=0)
+        borders_mask = cv2.Canny(blurred_image, 150, 200, None, 3)
         cropped_image = _filter_region_of_interest(borders_mask)
-        line = cv2.HoughLinesP(cropped_image, rho=1, theta=math.pi/180, threshold=10, lines=None,
-                                minLineLength=5, maxLineGap=100)
-        if line is not None:
-            line = line[0]
-            cv2.line(cropped_image, (line[0][0], line[0][1]), (line[0][2], line[0][3]), (127,))
-            angle = get_angle_from_road_line(line)
-            print(angle)
+        lines = cv2.HoughLinesP(cropped_image, rho=5, theta=math.pi/180, threshold=22, lines=None,
+                                minLineLength=1, maxLineGap=7)
+        if lines is not None:
+            for line in lines:
+                cv2.line(cropped_image, (line[0][0], line[0][1]), (line[0][2], line[0][3]), (127,))
+            angle = get_angle_from_road_line(lines)
+        else:
+            angle = 0
+        print(f"Angle: {angle * 180}")
         _display_image(display_view, cropped_image)
         driver.setSteeringAngle(angle)
         driver.setCruisingSpeed(speed)
 
 
-def get_angle_from_road_line(line: np.ndarray) -> float:
-    x1, y1, x2, y2 = line[0]
-    angle = (y2 - y1) / (x2 - x1)
-    return angle
+def get_angle_from_road_line(lines: np.ndarray) -> float:
+    angles = []
+    CAMERA_OFFSET = math.pi / 2
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        tan = (y2 - y1) / (x2 - x1)
+        angle = math.atan(tan)
+        if angle > 0:
+            angle -= CAMERA_OFFSET
+        elif angle < 0:
+            angle += CAMERA_OFFSET
+        print(angle * 180)
+        angles.append(angle)
+    if angles:
+        deviation_angle = -statistics.mean(angles)
+        return deviation_angle
+    return 0
 
 
 def _filter_region_of_interest(img: np.ndarray):
     img_height, img_width = img.shape
     centre_rectangle = np.array([[
-        [0.1 * img_width, img_height],
-        [img_width * 0.5, img_height * 0.35],
-        [0.9 * img_width, img_height]
+        [0 * img_width, img_height],
+        [img_width * 0.5, img_height * 0.6],
+        [1 * img_width, img_height]
     ]], dtype=np.int32)
     region_filter = np.zeros_like(img)
     PIXEL_VALUE = 255,
@@ -79,11 +82,14 @@ def _filter_region_of_interest(img: np.ndarray):
     return filtered_image
 
 def _display_image(display_view: Display, image: np.ndarray):
+    img_width = image.shape[1]
+    img_height = image.shape[0]
+    rgb_image = np.dstack((image, image, image))
     image_ref = display_view.imageNew(
-        np.dstack((image, image, image)).tobytes(),
+        rgb_image.tobytes(),
         Display.RGB,
-        width=image.shape[1],
-        height=image.shape[0])
+        width=img_width,
+        height=img_height)
     display_view.imagePaste(image_ref, 0, 0, False)
     display_view.imageDelete(image_ref)
 
